3685fb70eea708c01afb2092976e90baffcd12d3
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-10 15:17:29 -0500

    sets up server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000;
+
+// Required for our POST requests to work
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
