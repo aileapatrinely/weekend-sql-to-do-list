5b1d685aa7dcd0912b5d4c90964cd9b46d0f0e80
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-11 09:02:41 -0500

    creates get and post in router

--- server/routes/to-do.router.js
+++ server/routes/to-do.router.js
@@ -1,3 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+
+router.get('/', (req, res) => {
+  console.log(`In /tasks GET`);
+
+  let queryText = `SELECT * FROM "to-do" ORDER BY "complete" ASC;`;
+  pool
+    .query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error in GET /tasks ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
+  console.log(`In /tasks POST with`, req.body);
+
+  const tasktoAdd = req.body;
+  const queryText = `INSERT INTO "to-do" ("task", "complete")
+                         VALUES ($1, $2);`;
+  pool
+    .query(queryText, [taskToAdd.task, taskToAdd.complete])
+    .then((response) => {
+      console.log(response);
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error in POST /tasks ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
+const toDoRouter = require('./routes/to-do.router.js');
 
 // Required for our POST requests to work
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/tasks', toDoRouter);
 
 app.use(express.static('server/public'));
 
